6d1e18662757394f124291d3f52c55dfbac1d2e2
todd <todd@fadutec.com>
2022-02-14 16:09:51 +0900

    init: first

---
+++ db/connect.go
@@ -0,0 +1,26 @@
+package db
+
+import(
+	"os"
+
+	_ "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+)
+
+func Load()
+
+func Connect() *gorm.DB {
+    USER := os.Getenv("DB_USER")       // DB 유저명
+    PASS := os.Getenv("DB_PASS")       // DB 유저의 패스워드
+    PROTOCOL := "tcp(localhost:3306)" // 개발환경이므로 localhost의 3306포트로 설정한다.
+    DBNAME := os.Getenv("DB_NAME")     // 사용할 DB 명을 입력
+
+    CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
+    db, err := gorm.Open(mysql.Open(CONNECT), &gorm.Config{})
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    return db
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"log"
+)
+
+func main(){
+	log.Println("#############################")
+	log.Println("INSUNG.LOL - BACKEND")
+	log.Println("#############################")
+
+	
+}
